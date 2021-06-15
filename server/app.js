9f27aaafc59cc52a5b99ab68814337950683b820
Mrutyunjay Dash <mdash555@gmail.com>
2021-06-15 16:42:35 +0530

    init repo

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+dotenv.config({ path:'./config.env' });
+
+require('./db/conn');
+
+const PORT=process.env.PORT;
+
+
+app.get('/',(req,res) => {
+    res.send('Hello world from the server');
+});
+
+app.get('/about',(req,res) => {
+    res.send('This is about page');
+});
+
+app.get('/contact' , (req,res) => {
+    res.send('contact');
+})
+
+app.get('/signin',(req,res) => {
+    res.send('SignIN');
+});
+
+app.get('/signup',(req,res) => {
+    res.send('SignUp');
+});
+
+app.listen(PORT , () => {
+    console.log('server running at port ${PORT}');
+});
