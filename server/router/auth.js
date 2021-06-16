4ca37b984c8b1829eb37fbad8fcc88e02d24c5b1
Mrutyunjay Dash <mdash555@gmail.com>
2021-06-16 12:49:41 +0530

    async await added

--- server/router/auth.js
+++ server/router/auth.js
@@ -7,6 +7,8 @@ router.get('/',(req,res) => {
     res.send('Hello world from the server');
 });
 
+/*using promises
+
 router.post('/register',(req,res) => {
     const { name, email, phone, work, password, cpassword } = req.body;
     if (!name || !email || !phone || !work || !password || !cpassword){
@@ -26,6 +28,32 @@ router.post('/register',(req,res) => {
             res.status(500).json({error:'Registration failed'});
         }).catch(err => {console.log(err); });
     })
-});
+}); 
+*/
+
+//using async await
+
+router.post('/register', async(req,res) => {
+    const { name, email, phone, work, password, cpassword } = req.body;
+    if (!name || !email || !phone || !work || !password || !cpassword){
+        return res.status(422).json({error:"Please fill the details"});
+    }
+    try{
+        const emailExists = await User.findOne({email:email});
+        if(emailExists){
+            return res.status(422).json({error:'Email already exists'});
+        }
+
+        const user = new User({name, email, phone, work, password, cpassword});
+        await user.save();
+        res.status(200).json({message:'User registered successfully'});
+
+    }
+    catch(err){
+        console.log(err);
+    }
+    
+})
+
 
 module.exports = router;
