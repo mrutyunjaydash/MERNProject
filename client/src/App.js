8dfa707079e990f43252b9e16f099b7ec041e2ec
Mrutyunjay Dash <mdash555@gmail.com>
2021-06-21 21:32:06 +0530

    react router added to all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Contact from './components/Contact';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <>
         <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/Singup">
+          <Signup />
+        </Route>
       </>
     );
   }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <>
             
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand">Navbar</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -14,19 +15,19 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Home</a>
+                        <NavLink className="nav-link" to="/">Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">About</a>
+                        <NavLink className="nav-link" to="/about">About</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Contact</a>
+                        <NavLink className="nav-link" to="/contact">Contact</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Login</a>
+                        <NavLink className="nav-link" to="/login">Login</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Register</a>
+                        <NavLink className="nav-link" to="/signup">Register</NavLink>
                     </li>
                     </ul>
                 </div>
