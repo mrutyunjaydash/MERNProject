981b1817e52394b7632f3596887374e1f170ffe0
Mrutyunjay Dash <mdash555@gmail.com>
2021-06-30 20:27:29 +0530

    error page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
+import { Route,Switch } from "react-router-dom";
+import "./App.css";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Contact from './components/Contact';
-import { Route } from "react-router-dom";
-import "./App.css";
+import ErrorPage from './components/ErrorPage';
 
 class App extends Component {
   render() {
     return (
       <>
         <Navbar />
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+          <Route>
+            <ErrorPage />
+          </Route>
+        </Switch>
       </>
     );
   }
