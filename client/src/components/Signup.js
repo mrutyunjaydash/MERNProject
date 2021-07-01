d0e2b05c85500731858eac8f0ffb9f2be8044688
Mrutyunjay Dash <mdash555@gmail.com>
2021-07-01 20:47:58 +0530

    states added in signup

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 const Signup = () => {
+
+    const [ user,setUser ] = useState({
+        name:"" , email:"" , phone:"", work:"",password:"",cpassword:""
+    });
+
+    let name,value;
+
+    const handleChange = (e) => {
+        console.log(e);
+        name = e.target.name;
+        value = e.target.value;
+        setUser({...user, [name] : value})
+    }
     return (
         <div className="signup">
             <div className="container">
@@ -9,35 +22,65 @@ const Signup = () => {
                     <form className="center">
                         <div className="form-group">
                         <div className="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
+                            <input type="text" className="form-control" placeholder="Name" aria-label="Username"
+                            id="name" 
+                            name="name"
+                            value={ user.name }
+                            onChange={ handleChange }
+                            ></input>
                             </div>
                         </div>
                         <div className="form-group">
                         <div className="input-group mb-3">
-                            <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"></input>
+                            <input type="email" className="form-control" placeholder="Email" aria-label="Email" 
+                            id="email" 
+                            name="email"
+                            value={ user.email }
+                            onChange={ handleChange }
+                            ></input>
                             </div>
                         </div>
                         <div className="form-group">
                         <div className="input-group mb-3">
-                            <input type="number" class="form-control" placeholder="Phone" aria-label="Phone" aria-describedby="basic-addon1"></input>
+                            <input type="number" className="form-control" placeholder="Phone" aria-label="Phone" 
+                            id="phone" 
+                            name="phone"
+                            value={ user.phone }
+                            onChange={ handleChange }
+                            ></input>
                             </div>
                         </div>
                         <div className="form-group">
                         <div className="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Work" aria-label="Work" aria-describedby="basic-addon1"></input>
+                            <input type="text" className="form-control" placeholder="Work" aria-label="Work" 
+                            id="work" 
+                            name="work"
+                            value={ user.work}
+                            onChange={ handleChange }
+                            ></input>
                             </div>
                         </div>
                         <div className="form-group">
                         <div className="input-group mb-3">
-                            <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1"></input>
+                            <input type="password" className="form-control" placeholder="Password" aria-label="Password" 
+                            id="password" 
+                            name="password"
+                            value={ user.password }
+                            onChange={ handleChange }
+                            ></input>
                             </div>
                         </div>
                         <div className="form-group">
                         <div className="input-group mb-3">
-                            <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Confirm Password" aria-describedby="basic-addon1"></input>
+                            <input type="password" className="form-control" placeholder="Confirm Password" aria-label="Confirm Password" 
+                            id="cpassword" 
+                            name="cpassword"
+                            value={ user.cpassword }
+                            onChange={ handleChange }
+                            ></input>
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
                     </div>
                 </div>
