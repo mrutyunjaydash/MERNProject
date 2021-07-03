46e1351e8a97b20598879547a750c1feed552b15
Mrutyunjay Dash <mdash555@gmail.com>
2021-07-03 18:48:31 +0530

    login page linked with backend

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { useHistory } from 'react-router';
 
 const Login = () => {
+
+const  history = useHistory();
+
+const [email,setEmail] = useState('');
+const [password,setPassword] = useState('');
+
+const loginUser = async(e) => {
+    e.preventDefault();
+
+    const res = await fetch("/signin",{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body:JSON.stringify({ email,password })  
+    });
+
+    const data = await res.json();
+    if(data.status === 400 || !data){
+        window.alert("Invalid Credentials");
+    }
+    else{
+        window.alert("Login Successfull");
+        history.push("/");
+    }
+}
+
     return (
         <div className="login">
            <div className="container">
                <div className="row justify-content-md-center">
                    <div className="col col-lg-3">
-                   <form className="center">
+                   <form method="POST" className="center">
                         <div className="form-group">
                             <div className="input-group mb-3">
-                                <input type="email" class="form-control" placeholder="Enter Email" aria-label="Email" aria-describedby="basic-addon1"></input>
+                                <input type="email" class="form-control" placeholder="Enter Email" aria-label="Email" 
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                ></input>
                                 </div>
                         </div>
                         <div className="form-group">
                             <div className="input-group mb-3">
-                                <input type="password" class="form-control" placeholder="Enter Password" aria-label="password" aria-describedby="basic-addon1"></input>
+                                <input type="password" class="form-control" placeholder="Enter Password" aria-label="password" 
+                                name="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                ></input>
                                 </div>
                         </div>
-                        <button type="submit" class="btn btn-success">Login</button>
+                        <button type="submit" class="btn btn-success" 
+                        onClick={loginUser}
+                        >Login</button>
                    </form>
                    </div>
                </div>
